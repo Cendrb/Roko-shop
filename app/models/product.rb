474e7a1a46f8349cebeaa067030e812fee14cbb3
Cendrb <cendrb@gmail.com>
2013-12-30 09:54:20 +0100

    Vallidace oprava

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format =>{
-    :with => %r{\.(gif|jpg|png)$}i,
+    :with => %r{\.(gif|jpg|png)}i,
     :message => "Musí jít o adresu URL pro obrázek typu GIF, JPG nebo PNG."
   }
 end
