54af1fa23be4c529092cb24768cc6c009f058fa6
Cendrb <cendrb@gmail.com>
2014-01-02 19:21:35 +0100

    Pokusy s "testy a přípravky"

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format =>{
-    :with => %r{\.(gif|jpg|png)}i,
-    :message => "Musí jít o adresu URL pro obrázek typu GIF, JPG nebo PNG."
+    :with => %r{\.(gif|jpg|png)}i, :message => "AD."
   }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "product controllers must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -21,7 +22,7 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
     
     product.price = 1
-    assert product.invalid?
+    assert product.valid?
   end
   
   def new_product(image_url)
@@ -30,5 +31,12 @@ class ProductTest < ActiveSupport::TestCase
   
   test "image_url" do
     ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} by nemělo být neplatné"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} by nemělo být platné"
+    end
   end
 end
