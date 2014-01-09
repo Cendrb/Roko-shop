db9c44366ca97b16c151fc2e16cda14fe0d4531b
Cendrb <cendrb@gmail.com>
2014-01-09 21:10:21 +0100

    počátky GUI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RokoShop::Application.routes.draw do
+  get "store/index"
   get "penachim/mapa"
   get "penachim/fuckoff"
   get "penachim/rododendron"
@@ -9,6 +10,7 @@ RokoShop::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'store#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
