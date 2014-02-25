f9cfeaed6cf3d21e47f665be1a61953f4363caa3
Cendrb <cendrb@gmail.com>
2014-02-25 16:50:39 +0100

    penis

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,24 @@
 class Product < ActiveRecord::Base
-  default_scope { order('title') } 
-  
+  default_scope { order('title') }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format =>{
     :with => %r{\.(gif|jpg|png)}i, :message => "AD."
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+    return true
+    else
+      errors.add(:base, 'Exsitují položky')
+    return false
+    end
+  end
 end
