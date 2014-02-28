2eb8e0020c45e6e0265e3a7b9401525aeffcc90f
Cendrb <cendrb@gmail.com>
2014-02-28 17:49:53 +0100

    Depot F

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,13 +1,9 @@
- require 'test_helper'
+require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_select '#columns #side a', :minimum => 4
-    assert_select '#main .entry', 3
-    assert_select 'h3', 'Začínáme programovat v Ruby on Rails'
-    assert_select '.price', /[\s\d]+\,\d\d Kč/
   end
 
 end
