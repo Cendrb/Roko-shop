64a9fd84a773b5e7d49acaa7c5a2dcd5c9cc95b4
Cendrb <cendrb@gmail.com>
2014-03-04 23:21:54 +0100

    Heroku Postgresql test

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140228214947) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
