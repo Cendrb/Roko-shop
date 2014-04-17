b20b27017daf8ef381fea40ec53cd4b0acc36f2b
Cendrb <cendrb@gmail.com>
2014-04-17 14:28:11 +0200

    [FEATURE] Integration test

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -2,6 +2,60 @@ require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
   fixtures :products
-  
-  
+
+  test "buying a product" do
+    LineItem.delete_all # clear LI
+    Order.delete_all # clear orders
+    ruby_book = products(:ruby)
+    
+    # Visit index page
+    get "/" # get index site
+    assert_response :success # verify response
+    assert_template "index" # verify content
+    
+    # Add item to cart
+    xml_http_request :post, '/line_items', product_id: ruby_book.id # send request
+    assert_response :success # verify response
+    
+    # Verify if item is in cart
+    cart = Cart.find(session[:cart_id]) # get cart
+    assert_equal 1, cart.line_items.size # verify number of items in cart
+    assert_equal ruby_book, cart.line_items[0].product # is ruby book in cart?
+    
+    # Visit new order
+    get "/orders/new" # get new order
+    assert_response :success # verify response
+    assert_template "new" # verify content
+    
+    # Send form
+    post_via_redirect "/orders", order: { name: "Ivona Součková", address: "Kurví díra 69/139", email: "kurva@laris.ad", pay_type: "Korupce" } # Send POST request
+    assert_response :success # verify response
+    assert_template "index" # verify content
+    
+    # Verify items
+    cart = Cart.find(session[:cart_id]) # get cart
+    assert_equal 0, cart.line_items.size # verify number of items in cart
+    
+    # Verify order
+    orders = Order.all
+    assert_equal 1, orders.size # verify number of orders
+    order = orders[0]
+    
+    # Verify data in order
+    assert_equal "Ivona Součková", order.name
+    assert_equal "Kurví díra 69/139", order.address
+    assert_equal "kurva@laris.ad", order.email
+    assert_equal "Korupce", order.pay_type
+    
+    # Verify items in order
+    assert_equal 1, order.line_items.size # verify number of items in order
+    line_item = order.line_items[0]
+    assert_equal ruby_book, line_item.product # verify item in order
+    
+    # Verify mail
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["kurva@laris.ad"], mail.to
+    assert_equal ["Pragmatická Ivona"], mail.from
+    assert_equal "Potvrzení objednávky z obchodu Pragmatická Ivona", mail.subject
+  end
 end
--- test/mailers/notifier_test.rb
+++ test/mailers/notifier_test.rb
@@ -5,14 +5,14 @@ class NotifierTest < ActionMailer::TestCase
     mail = Notifier.order_received(orders(:one))
     assert_equal "Potvrzení objednávky z obchodu Pragmatická Ivona", mail.subject
     assert_equal ["penis@gmail.com"], mail.to
-    assert_equal ["Pragamatická Ivona"], mail.from
+    assert_equal ["Pragmatická Ivona"], mail.from
   end
 
   test "order_shipped" do
     mail = Notifier.order_shipped(orders(:one))
     assert_equal "Objednávka z obchodu Pragmatická Ivona byla odeslána", mail.subject
     assert_equal ["penis@gmail.com"], mail.to
-    assert_equal ["Pragamatická Ivona"], mail.from
+    assert_equal ["Pragmatická Ivona"], mail.from
   end
 
 end
